861fe6252983077122b940abbb048ef48f3c3ae7
Yux <lin.yihang@outlook.de>
2023-11-07 16:02:31 +0100

    vercel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
     //vercel
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
+
     inject({ mode: dev ? 'development' : 'production' });
 
     let isActive: boolean = true;
