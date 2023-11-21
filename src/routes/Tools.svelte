2f007630fd122865f01ffb22e7dc0cdad61a7b07
Yux <lin.yihang@outlook.de>
2023-11-21 22:10:03 +0100

    trashed not usable tw utility classes

--- src/routes/Tools.svelte
+++ src/routes/Tools.svelte
@@ -21,7 +21,6 @@
             { src: "media/svelte.png", alt: "svelte"},
             { src: "media/CSS.png", alt: "CSS" },
 
-
         ],
         [
             { src: "media/php.png", alt: "PHP" },
@@ -117,11 +116,11 @@
 <div class="pr-5 lg:pl-14 pl-5 big-screens big-screen:mt-[-18rem] big-screen:w-[30rem] md:w-[30rem] lg:w-[30rem] " style={style}> <!-- big screen(s) is a variable for ny media css-->
     <div class="grid grid-cols-1">
         {#each technologies as tech, index (index)}
-            <div class="mb-4 inline-block border-2 border-black rounded-md p-4">
-                <h2 class="p-2 inline-block text-white ">{tech}</h2>
+            <div class="mb-4 border-2 border-black rounded-md p-4">
+                <h2 class="p-2 text-white ">{tech}</h2>
                 <div class="flex flex-row flex-wrap gap-x-2 gap-y-2">
                     {#each technologyImages[index] as image}
-                        <img src={image.src} alt={image.alt} class="w-auto">
+                        <img src={image.src} alt={image.alt}>
                     {/each}
                 </div>
             </div>
